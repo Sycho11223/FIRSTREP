0c89d6944f02a8aa6f732d4d932b5246e3ca0bfe
Sycho11223 <guthulavenkatesh6999@gmail.com>
2023-02-14 09:19:44 +0530

    End of Section 2

---
+++ FIRSTREP/FIRSTREP.API/Data/FIRSTREPDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FIRSTREP.API.Models.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIRSTREP.API.Data
+{
+    public class FIRSTREPDbContext:DbContext
+    {
+        public FIRSTREPDbContext(DbContextOptions<FIRSTREPDbContext>options): base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ FIRSTREP/FIRSTREP.API/Models/Domains/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace FIRSTREP.API.Models.Domains
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }    
+    }
+}
